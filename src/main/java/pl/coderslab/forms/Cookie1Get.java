04a44a35c19767381ad6f908f0482388e1c4d50e
Griegoriens <skwarnel@gmail.com>
2022-10-01 12:55:11 +0200

    Work temporarily save

--- src/main/java/pl/coderslab/forms/Cookie1Get.java
+++ src/main/java/pl/coderslab/forms/Cookie1Get.java
@@ -1,2 +1,27 @@
-package pl.coderslab.forms;public class Cookie1Get {
+package pl.coderslab.forms;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebServlet("/showCookie")
+public class Cookie1Get extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies =  req.getCookies();
+        if (Objects.nonNull(cookies)) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("User")) {
+                    resp.getWriter().println(cookie.getValue());
+                    resp.getWriter().println(cookie.getDomain());
+                    resp.getWriter().println(cookie.getMaxAge());
+                }
+            }
+        }
+    }
 }
